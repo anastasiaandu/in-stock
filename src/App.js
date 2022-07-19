c100fd152e4a61d772ad4045fe7428269b28ed90
Saloni Jindal <salonijindal@MacBook-Pro.local>
2022-07-19 01:31:59 -0400

    Basic set up of routes for warehouse Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import WarehousePage from "./pages/WarehousePage";
+//import InventoryPage from "./pages/InventoryPage";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={WarehousePage} />
+        <Route path="/warehouses/:id" component={WarehousePage} />
+        <Route path="/warehouses/:id/edit" component={WarehousePage} />
+        <Route path="/warehouses/add" component={WarehousePage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
