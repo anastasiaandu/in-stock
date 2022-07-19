f65cc8d51c403fa8946bc18a6b5201f5b8cde4eb
Bolawole <andubolawole@gmail.com>
2022-07-18 22:53:38 -0400

    Add the logo and navigation to the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import PageHeader from './components/PageHeader/PageHeader';
+import PageFooter from './components/PageFooter/PageFooter';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <PageHeader />
+      <PageFooter />
+    </Router>
   );
 }
 
--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,10 +1,35 @@
+import { NavLink } from 'react-router-dom';
 import './PageHeader.scss';
+import logoImage from '../../assets/logos/InStock-Logo.svg'
 
 const PageHeader = () => {
     return (
-        <div>
-            
-        </div>
+        <header className='header'>
+            <img src={logoImage} alt='in-stock logo' className='header__logo'/>
+            <nav>
+                <ul className='header__nav'>
+                    <li>
+                        <NavLink
+                            to='/'
+                            exact
+                            className='header__link header__link--warehouses'
+                            activeClassName='header__link--active'
+                        >
+                            Warehouses
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/'
+                            className='header__link'
+                            activeClassName='header__link--active'
+                        >
+                            Inventory
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
