03db0431588d109d7c96b2b99c12913b90d4a225
theonlyouan <juanjovanegas21@gmail.com>
2022-07-20 16:50:28 -0400

    Connect GET single warehouse API endpoint

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -1,78 +1,122 @@
 import DetailsListItem from "../DetailsListItem/DetailsListItem";
 import NavigationHeader from "../NavigationHeader/NavigationHeader";
 import sortIcon from "../../assets/icons/sort-24px.svg";
+import { Component } from "react";
+import axios from "axios";
 import "./WarehouseDetails.scss";
 
-const WarehouseDetails = (props) => {
-  return (
-    <article className="warehouse-details">
-      <NavigationHeader title="Washington" showEditButton={true} />
-      <section className="warehouse-info">
-        <div className="warehouse-info__address">
-          <h4>Warehouse address:</h4>
-          <p2>300 Pearl Street SW,</p2>
-          <p2>Washington, USA</p2>
-        </div>
-        <div className="warehouse-info__contact-name">
-          <h4>Contact name:</h4>
-          <p2>Graeme Lyon</p2>
-          <p2>Warehouse Manager</p2>
-        </div>
-        <div className="warehouse-info__contact-info">
-          <h4>Contact information:</h4>
-          <p2>+1 (647) 504 - 0911</p2>
-          <p2>glyon@instock.com</p2>
-        </div>
-      </section>
-      <section className="warehouse-sort-header">
-        <div className="warehouse-sort-header__section">
-          <h4>Inventory Item</h4>
-          <img
-            className="warehouse-sort-header__sort-icon"
-            src={sortIcon}
-            alt="sort"
-          />
-        </div>
-        <div className="warehouse-sort-header__section">
-          <h4>Category</h4>
-          <img
-            className="warehouse-sort-header__sort-icon"
-            src={sortIcon}
-            alt="sort"
-          />
-        </div>
-        <div className="warehouse-sort-header__section">
-          <h4>Status</h4>
-          <img
-            className="warehouse-sort-header__sort-icon"
-            src={sortIcon}
-            alt="sort"
-          />
-        </div>
-        <div className="warehouse-sort-header__section">
-          <h4>Quantity</h4>
-          <img
-            className="warehouse-sort-header__sort-icon"
-            src={sortIcon}
-            alt="sort"
-          />
-        </div>
-        <h4>Actions</h4>
-      </section>
-      <DetailsListItem
-        inventoryItem="Television"
-        status="In stock"
-        category="Electronics"
-        quantity={500}
-      />
-      <DetailsListItem
-        inventoryItem="Gym Bag"
-        status="Out of stock"
-        category="Gear"
-        quantity={0}
-      />
-    </article>
-  );
-};
+class WarehouseDetails extends Component {
+  state = {
+    address: null,
+    city: null,
+    country: null,
+    contact: null,
+    id: null,
+    inventory: null,
+    name: null,
+    isLoading: true,
+  };
+  componentDidMount() {
+    console.log("WarehouseDetails mounted");
+    axios
+      .get(
+        `http://localhost:8080/warehouses/${this.props.warehouseId}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          address: response.data.address,
+          city: response.data.city,
+          country: response.data.country,
+          contact: response.data.contact,
+          id: response.data.id,
+          inventory: response.data.inventory,
+          name: response.data.name,
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        console.log(
+          "There was an error fetching the warehouse details: ",
+          error
+        );
+      });
+  }
+  render() {
+    return this.state.isLoading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <article className="warehouse-details">
+        <NavigationHeader title={this.state.name} showEditButton={true} />
+        <section className="warehouse-info">
+          <div className="warehouse-info__address">
+            <h4>Warehouse address:</h4>
+            <p2>{this.state.address}</p2>
+            <p2>
+              {this.state.city}, {this.state.country}
+            </p2>
+          </div>
+          <div className="warehouse-info__contact-name">
+            <h4>Contact name:</h4>
+            <p2>{this.state.contact.name}</p2>
+            <p2>{this.state.contact.position}</p2>
+          </div>
+          <div className="warehouse-info__contact-info">
+            <h4>Contact information:</h4>
+            <p2>{this.state.contact.phone}</p2>
+            <p2>{this.state.contact.email}</p2>
+          </div>
+        </section>
+        <section className="warehouse-sort-header">
+          <div className="warehouse-sort-header__section">
+            <h4>Inventory Item</h4>
+            <img
+              className="warehouse-sort-header__sort-icon"
+              src={sortIcon}
+              alt="sort"
+            />
+          </div>
+          <div className="warehouse-sort-header__section">
+            <h4>Category</h4>
+            <img
+              className="warehouse-sort-header__sort-icon"
+              src={sortIcon}
+              alt="sort"
+            />
+          </div>
+          <div className="warehouse-sort-header__section">
+            <h4>Status</h4>
+            <img
+              className="warehouse-sort-header__sort-icon"
+              src={sortIcon}
+              alt="sort"
+            />
+          </div>
+          <div className="warehouse-sort-header__section">
+            <h4>Quantity</h4>
+            <img
+              className="warehouse-sort-header__sort-icon"
+              src={sortIcon}
+              alt="sort"
+            />
+          </div>
+          <h4>Actions</h4>
+        </section>
+        {this.state.inventory.map((item) => {
+          return (
+            <DetailsListItem
+              key={item.id}
+              id={item.id}
+              inventoryItem={item.itemName}
+              category={item.category}
+              status={item.status}
+              quantity={item.quantity}
+            />
+          );
+        })}
+      </article>
+    );
+  }
+}
 
 export default WarehouseDetails;
