fa7df3fff3ebb2f27591d6512c730ed7915863f3
theonlyouan <juanjovanegas21@gmail.com>
2022-07-22 16:11:17 -0400

    Convert App.js back to functional component

--- src/App.js
+++ src/App.js
@@ -5,26 +5,23 @@ import PageFooter from "./components/PageFooter/PageFooter";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
-import { Component } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <PageHeader />
-        <div className="main-container">
-          <Switch>
-            <Route path="/" exact component={WarehousePage} />
-            <Route path="/warehouses/:id" component={WarehouseDetailsPage} />
-            <Route path="/warehouses/:id/edit" component={WarehousePage} />
-            <Route path="/warehouses/add" component={WarehousePage} />
-            <Route path="/inventory" component={InventoryPage} />
-          </Switch>
-        </div>
-        <PageFooter />
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <PageHeader />
+      <div className="main-container">
+        <Switch>
+          <Route path="/" exact component={WarehousePage} />
+          <Route path="/warehouses/:id" component={WarehouseDetailsPage} />
+          <Route path="/warehouses/:id/edit" component={WarehousePage} />
+          <Route path="/warehouses/add" component={WarehousePage} />
+          <Route path="/inventory" component={InventoryPage} />
+        </Switch>
+      </div>
+      <PageFooter />
+    </BrowserRouter>
+  );
+};
 
 export default App;
