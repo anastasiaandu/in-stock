62a7f36576cc9c092ea051f8546b5c008a74670d
Saloni Jindal <salonijindal@MacBook-Pro.ht.home>
2022-07-22 05:39:28 -0400

    Add state, validations, errorState, styling related to error, handle forms methods, and axios requests

--- src/components/WarehouseForm/WarehouseForm.js
+++ src/components/WarehouseForm/WarehouseForm.js
@@ -1,50 +1,122 @@
 import React, { Component } from "react";
 import NavigationHeader from "../NavigationHeader/NavigationHeader";
 import "./WarehouseForm.scss";
+import axios from "axios";
+import errorImg from "../../assets/icons/error-24px.svg";
 class WarehouseForm extends Component {
   state = {
-    warehouseName: "Washington",
-    address: "33 Pearl Street SW",
-    city: "Washington",
+    id: "2922c286-16cd-4d43-ab98-c79f698aeab0",
+    name: "Manhattan",
+    address: "503 Broadway",
+    city: "New York",
     country: "USA",
-    contactName: "Saloni Jindal",
-    position: "Developer",
-    phoneNumber: "5197298285",
-    email: "sgs@gmail.com",
+    contact: {
+      name: "Parmin Aujla",
+      position: "Warehouse Manager",
+      phone: "+1 (519) 729-8285",
+      email: "paujla@instock.com",
+    },
+    errorField: {
+      name: false,
+      address: false,
+      city: false,
+      country: false,
+      contactName: false,
+      position: false,
+      phone: false,
+      email: false,
+    },
   };
   handleSubmit = (e) => {
     e.preventDefault();
-    if (
-      !this.state.warehouseName ||
-      !this.state.address ||
-      !this.state.city ||
-      !this.state.country ||
-      !this.state.contactName ||
-      !this.state.position ||
-      !this.state.phoneNumber ||
-      !this.state.email
-    ) {
-      return alert("Please enter all the required field");
+    if (!this.state.name) {
+      return this.setState({
+        errorField: {
+          name: true,
+        },
+      });
     }
+    if (!this.state.address) {
+      return this.setState({
+        errorField: {
+          address: true,
+        },
+      });
+    }
+    if (!this.state.city) {
+      return this.setState({
+        errorField: {
+          city: true,
+        },
+      });
+    }
+    if (!this.state.country) {
+      return this.setState({
+        errorField: {
+          country: true,
+        },
+      });
+    }
+    if (!this.state.contact.name) {
+      return this.setState({
+        errorField: {
+          contactName: true,
+        },
+      });
+    }
+    if (!this.state.contact.position) {
+      return this.setState({
+        errorField: {
+          position: true,
+        },
+      });
+    }
+    if (!this.state.contact.phone) {
+      return this.setState({
+        errorField: {
+          phone: true,
+        },
+      });
+    }
+    if (!this.state.contact.email) {
+      return this.setState({
+        errorField: {
+          email: true,
+        },
+      });
+    }
+
     console.log("Submitted value =====>", this.state);
-    // axios
-    //   .patch(`${SERVER_URL}/ware`, this.state)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-    // alert("Your video has been submitted!");
+    axios
+      .patch(`http://localhost:8000/warehouses/${this.state.id}`, this.state)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    alert("Detail has been updated!");
   };
 
-  handleChange = (e) => {
-    const target = e.target;
-    const value = target.name;
+  handleChange = (contact) => (e) => {
+    if (!contact) {
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    } else {
+      this.setState((prevState) => ({
+        contact: {
+          ...prevState.contact,
+          [e.target.name]: e.target.value,
+        },
+      }));
+    }
+  };
+  handleFocus = (value) => (e) => {
     this.setState({
-      [value]: e.target.value,
+      errorField: { [value]: false },
     });
-    console.log(this.state);
+    e.target.removeAttribute(e.target.id);
   };
   render() {
     return (
@@ -54,47 +126,123 @@ class WarehouseForm extends Component {
           <div className="warehouse-form__container">
             <section className="warehouse-form__input-panel">
               <h2 className="warehouse-form__title">Warehouse Details</h2>
-              <label htmlFor="warehouse-name" name="warehouse-name">
+              <label htmlFor="name" name="name">
                 Warehouse Name
               </label>
               <input
+                onFocus={this.handleFocus("name")}
                 className="warehouse-form__input"
+                id={`${
+                  this.state.errorField.name && "warehouse-form__error-border"
+                }`}
                 type="text"
-                onChange={this.handleChange}
-                value={this.state.warehouseName}
-                name="warehouse-name"
-                placeholder={this.state.warehouseName}
+                onChange={this.handleChange("")}
+                value={this.state.name}
+                name="name"
+                placeholder={
+                  this.state.name ? this.state.name : "Warehouse Name"
+                }
               ></input>
+              {this.state.errorField.name && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
+
               <label htmlFor="address" name="address">
                 Street Address
               </label>
               <input
+                onFocus={this.handleFocus("address")}
+                id={`${
+                  this.state.errorField.address &&
+                  "warehouse-form__error-border"
+                }`}
                 className="warehouse-form__input"
                 type="text"
-                onChange={this.handleChange}
+                onChange={this.handleChange("")}
                 value={this.state.address}
                 name="address"
+                placeholder={
+                  this.state.address ? this.state.address : "Street Address"
+                }
               ></input>
+              {this.state.errorField.address && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
               <label htmlFor="city" name="city">
                 City
               </label>
               <input
+                onFocus={this.handleFocus("city")}
                 className="warehouse-form__input"
+                id={`${
+                  this.state.errorField.city && "warehouse-form__error-border"
+                }`}
                 type="text"
                 name="city"
                 value={this.state.city}
-                onChange={this.handleChange}
+                onChange={this.handleChange("")}
+                placeholder={this.state.city ? this.state.city : "City"}
               ></input>
+              {this.state.errorField.city && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
               <label htmlFor="country" name="country">
                 Country
               </label>
               <input
+                onFocus={this.handleFocus("country")}
+                id={`${
+                  this.state.errorField.country &&
+                  "warehouse-form__error-border"
+                }`}
                 className="warehouse-form__input"
                 type="text"
                 name="country"
                 value={this.state.country}
-                onChange={this.handleChange}
+                onChange={this.handleChange("")}
+                placeholder={
+                  this.state.country ? this.state.country : "Country"
+                }
               ></input>
+              {this.state.errorField.country && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
             </section>
             <section className="warehouse-form__input-panel">
               <h2 className="warehouse-form__title">Contact Details</h2>
@@ -102,42 +250,126 @@ class WarehouseForm extends Component {
                 Contact Name
               </label>
               <input
+                onFocus={this.handleFocus("contactName")}
+                id={`${
+                  this.state.errorField.contactName &&
+                  "warehouse-form__error-border"
+                }`}
                 className="warehouse-form__input"
                 type="text"
-                onChange={this.handleChange}
-                name="contact-name"
-                value={this.state.contactName}
+                onChange={this.handleChange("contact")}
+                name="name"
+                value={this.state.contact.name}
+                placeholder={
+                  this.state.contact.name
+                    ? this.state.contact.name
+                    : "Contact Name"
+                }
               ></input>
+              {this.state.errorField.contactName && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
               <label htmlFor="position" name="position">
                 Position
               </label>
               <input
+                onFocus={this.handleFocus("position")}
+                id={`${
+                  this.state.errorField.position &&
+                  "warehouse-form__error-border"
+                }`}
                 className="warehouse-form__input"
                 type="text"
-                onChange={this.handleChange}
+                onChange={this.handleChange("contact")}
                 name="position"
-                value={this.state.position}
+                value={this.state.contact.position}
+                placeholder={
+                  this.state.contact.position
+                    ? this.state.contact.position
+                    : "Position"
+                }
               ></input>
-              <label htmlFor="phone-number" name="phone-number">
+              {this.state.errorField.position && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
+              <label htmlFor="phone" name="phone">
                 Phone Number
               </label>
               <input
+                onFocus={this.handleFocus("phone")}
+                id={`${
+                  this.state.errorField.phone && "warehouse-form__error-border"
+                }`}
                 className="warehouse-form__input"
                 type="text"
-                name="phone-number"
-                value={this.state.phoneNumber}
-                onChange={this.handleChange}
+                name="phone"
+                value={this.state.contact.phone}
+                onChange={this.handleChange("contact")}
+                placeholder={
+                  this.state.contact.phone
+                    ? this.state.contact.phone
+                    : "Phone Number"
+                }
               ></input>
+              {this.state.errorField.phone && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
               <label htmlFor="email" name="email">
                 Email
               </label>
               <input
+                onFocus={this.handleFocus("email")}
+                id={`${
+                  this.state.errorField.email && "warehouse-form__error-border"
+                }`}
                 className="warehouse-form__input"
                 type="text"
-                onChange={this.handleChange}
+                onChange={this.handleChange("contact")}
                 name="email"
-                value={this.state.email}
+                value={this.state.contact.email}
+                placeholder={
+                  this.state.contact.email ? this.state.contact.email : "Email"
+                }
               ></input>
+              {this.state.errorField.email && (
+                <div className="warehouse-form__error-container">
+                  <img
+                    src={errorImg}
+                    alt="Error Asteric"
+                    className="warehouse-form__error-icon"
+                  />
+                  <p3 className="warehouse-form__error-text">
+                    This field is required
+                  </p3>
+                </div>
+              )}
             </section>
           </div>
 
