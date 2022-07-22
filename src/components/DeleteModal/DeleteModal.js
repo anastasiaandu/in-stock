5b5aa2ed817e37594872f2ed250047f9d38e94c9
theonlyouan <juanjovanegas21@gmail.com>
2022-07-22 16:03:46 -0400

    Add modal responsiveness. Deactivate background scroll

--- src/components/DeleteModal/DeleteModal.js
+++ src/components/DeleteModal/DeleteModal.js
@@ -1,47 +1,69 @@
+import { Component } from "react";
 import closeIcon from "../../assets/icons/close-24px.svg";
 import "./DeleteModal.scss";
 
-const DeleteModal = (props) => {
-  if (!props.show) {
-    return null;
+class DeleteModal extends Component {
+  handleConfirmDeletion = () => {
+    this.props.onConfirmDelete(this.props.id);
+  };
+
+  componentDidUpdate() {
+    if (this.props.show) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
   }
 
-  const handleConfirmDeletion = () => {
-    props.onConfirmDelete(props.id);
-  };
-  return (
-    <div className="modal" onClick={props.onClose}>
-      <div
-        className="modal__content"
-        onClick={(event) => event.stopPropagation()}
-      >
-        <div>
-          <div className="modal__close">
-            <img src={closeIcon} alt="close icon" onClick={props.onClose} />
-          </div>
+  render() {
+    if (!this.props.show) {
+      return null;
+    }
+    return (
+      <div className="modal" onClick={this.props.onClose}>
+        <div
+          className="modal__content"
+          onClick={(event) => event.stopPropagation()}
+        >
+          <div>
+            <div className="modal__close">
+              <img
+                className="modal__close-icon"
+                src={closeIcon}
+                alt="close icon"
+                onClick={this.props.onClose}
+              />
+            </div>
 
-          <h1 className="modal__question">
-            Delete {props.title}{" "}
-            {props.itemType === "warehouse" ? "warehouse" : "inventory item"}?
-          </h1>
-          <p>
-            Please confirm that you'd like to delete {props.title} from{" "}
-            {props.itemType === "warehouse"
-              ? "the list of warehouses"
-              : "the inventory list"}
-            . You won't be able to undo this action.
-          </p>
-        </div>
-        <div className="modal__actions">
-          <button className="modal__cancel" onClick={props.onClose}>
-            Cancel
-          </button>
-          <button className="modal__confirm" onClick={handleConfirmDeletion}>
-            Delete
-          </button>
+            <h1 className="modal__question">
+              Delete {this.props.title}{" "}
+              {this.props.itemType === "warehouse"
+                ? "warehouse"
+                : "inventory item"}
+              ?
+            </h1>
+            <p>
+              Please confirm that you'd like to delete {this.props.title} from{" "}
+              {this.props.itemType === "warehouse"
+                ? "the list of warehouses"
+                : "the inventory list"}
+              . You won't be able to undo this action.
+            </p>
+          </div>
+          <div className="modal__actions">
+            <button className="modal__cancel" onClick={this.props.onClose}>
+              Cancel
+            </button>
+            <button
+              className="modal__confirm"
+              onClick={this.handleConfirmDeletion}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default DeleteModal;
